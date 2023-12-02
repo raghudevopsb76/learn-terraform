30f69542b807b29d53980e6e27217ee843026b44
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-02 06:21:09 +0530

    Prometheus ANsible Change

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -31,3 +31,8 @@ output "fruits_apple_quantity_with_name" {
   value = "Fruit Apple is having only ${var.fruits_with_quantity["apple"]} quantity"
 }
 
+variable "test1" {}
+output "test1" {
+  value = var.test1
+}
+
