7d6eb739abaa331576d0133e14e671f950e7026d
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-02 06:45:05 +0530

    Prometheus ANsible Change

--- 03-loops/main.tf
+++ 03-loops/main.tf
@@ -14,4 +14,20 @@ resource "null_resource" "sample" {
   count = length(var.instances)
 }
 
+variable "instances1" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
 
