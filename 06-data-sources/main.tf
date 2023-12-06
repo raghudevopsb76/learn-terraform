74cdf56a4c82ade299a1a9c0cea1baa69816b623
Raghu K <raghuk.vit@gmail.com>
2023-12-06 07:10:19 +0530

    Prometheus ANsible Change

--- 06-data-sources/main.tf
+++ 06-data-sources/main.tf
@@ -1,8 +1,22 @@
-data "aws_ssm_parameter" "user" {
-  name = "dev.rds.username"
+#data "aws_ssm_parameter" "user" {
+#  name = "dev.rds.username"
+#}
+#
+#output "username" {
+#  value = data.aws_ssm_parameter.user.value
+#}
+#
+
+data "aws_instance" "instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["workstation"]
+  }
 }
 
-output "username" {
-  value = data.aws_ssm_parameter.user.value
+output "instance" {
+  value = "Instance ID - ${data.aws_instance.instance.id} | Public IP - ${data.aws_instance.instance.public_ip}"
 }
+
+
 
