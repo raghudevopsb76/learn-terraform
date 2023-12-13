98eb0b93c99b4f4919275dc670c45c1d9a87ac69
Raghu K <raghuk.vit@gmail.com>
2023-12-13 07:02:41 +0530

    TF Code

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -11,7 +11,12 @@ variable "instance_type" {
   default = null
 }
 
+variable "create_instance" {
+  default = false
+}
+
 resource "aws_instance" "test" {
+  count         = 0
   ami           = "ami-03265a0778a880afb"
   instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
 }
